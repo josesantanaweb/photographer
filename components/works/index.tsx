ba0e0c5ea3c98d6033df30679167a63998f0672e
josesantana.web <josesantana.web@gmail.com>
2023-09-17 13:43:29 -0400

    feat: add active state

--- components/works/index.tsx
+++ components/works/index.tsx
@@ -4,6 +4,7 @@ import { tag, works } from "@/data";
 
 const Works = () => {
   const [worksData, setWorksData] = useState(works);
+  const [tagActive, setTagActive] = useState("");
 
   const filterTag = (tag: string) => {
     if (tag === "all") {
@@ -11,33 +12,36 @@ const Works = () => {
     } else {
       setWorksData(works.filter((item) => item.tag === tag));
     }
+    setTagActive(tag);
   };
 
   return (
     <section
-      className="flex items-center justify-center transition-all py-28 bg-white"
+      className="flex items-center justify-center transition-all bg-white py-28"
       id="works"
     >
       <div className="container px-20">
-        <div className="border-b border-gray-300 w-2/5 mb-4">
+        <div className="w-2/5 mb-4 border-b border-gray-300">
           <h3 className="mb-6 text-3xl font-bold text-primary">My Works</h3>
         </div>
         <div className="flex items-center gap-5 mb-5">
-          {tag.map((item, index) => (
-            <li
+          {tag.map((item: any, index: number) => (
+            <button
               key={index}
-              className="text-md text-gray-400 list-none hover:text-primary cursor-pointer capitalize"
+              className={`text-md list-none hover:text-primary cursor-pointer capitalize ${
+                item === tagActive ? 'text-primary font-semibold' : 'text-gray-400'
+              }`}
               onClick={() => filterTag(item)}
             >
               {item}
-            </li>
+            </button>
           ))}
         </div>
 
-        <div className="columns-3 gap-5">
+        <div className="gap-5 columns-3">
           {worksData.map((item, index) => (
             <div
-              className="grid grid-rows-1 mb-4 break-inside-avoid transition-all"
+              className="grid grid-rows-1 mb-4 transition-all break-inside-avoid"
               key={index}
             >
               <img
